c10e2bd3c1661d5112f6204aec123c7ceafea982
Michael DeCrescenzo <mdecrescenzo@drwholdings.com>
2021-12-10 12:27:42 -0600

    day 3: test data + pt 1 done

--- src/03.jl
+++ src/03.jl
@@ -1,86 +1,26 @@
 using DelimitedFiles
 using StatsBase
 
-report_raw = readdlm("data/03.txt", String) # matrix of strings (rows)
-report = [parse.(Int, x) for x in collect.(report_raw)] # matrix of int vectors
+# matrix of strings (rows)
+# report_raw = readdlm("data/03.txt", String) 
+report_raw = readdlm("data/03_test.txt", String) 
 
-reshape(vcat(report...), (length(report), length(report[1])))
-
-
-reshape(report, length(report), length(report[1]))
-
-A
-
-A = [1 2
-     3 4]
-
-A
-
-maximum.(eachcol(A))
-
-
-parse.(report, Char)
-
-parse.(Int, report_raw)
-
-collect.(report_raw)
-
-for i in eachrow(report_raw)
-    print(parse(Char, i))
+# convert to matrix of single chars
+report = let A = report_raw
+    A = collect.(A)               # explode string to chars
+    A = reshape(vcat(A...), :, length(A))  # matrix of chars BY COLUMN
+    permutedims(A)
 end
 
-report_raw[1]
+# --- pt 1 ---
 
-report = 
+modes = mode.(eachcol(report))
+anti_modes = [Dict('0' => '1', '1' => '0')[key] for key in modes]
+modes_as_string = String.([modes, anti_modes])
+# bits to ints (Julia greek compatibility, baybee)
+γ, ε = parse.(Int, modes_as_string, base=2)
 
-function add_spaces(x::SubArray{String})
-    print(x)
-    output = ""
-    for c in x
-        output += c * " "
-    end
-    return output
-    # x = c * ' ' for c in x
-    # # print(x)
-    # return x
-end
-
-first(eachrow(test_data))
-add_spaces(first(eachrow(test_data))[1])
+answer_1 = prod([γ, ε])
 
 
-
-γ
-ε
-
-power = γ * ε
-
-
-test_raw = ["00100"
-             "11110"
-             "10110"
-             "10111"
-             "10101"
-             "01111"
-             "00111"
-             "11100"
-             "10000"
-             "11001"
-             "00010"
-             "01010"]
-test_array = [parse.(Int, x) for x in collect.(test_raw)] # matrix of ints
-
-test_data = permutedims(reshape(hcat(test_array...), (length(test_array[1]), length(test_array))))
-# test_data = reshape(vcat(test_array...), (length(test_array), length(test_array[1])))
-
-test_data
-
-ints = mode.(eachcol(test_data))
-parse(Int, bitstring(BitArray(ints)), base=2)
-
-parse(Int, "110101"; base=2)
-
-
-
-
-first(eachrow(test_data))
+# --- pt 2 ---
