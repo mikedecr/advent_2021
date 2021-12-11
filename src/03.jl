beff2d5ae98875525e25993beb8cbefa713c5013
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2021-12-11 14:44:42 -0600

    finish 3 as one big ugly function

--- src/03.jl
+++ src/03.jl
@@ -1,9 +1,10 @@
 using DelimitedFiles
 using StatsBase
+using Pipe
 
-# matrix of strings (rows)
-# report_raw = readdlm("data/03.txt", String) 
-report_raw = readdlm("data/03_test.txt", String) 
+# matrix of strings
+report_raw = readdlm("data/03.txt", String) 
+# report_raw = readdlm("data/03_test.txt", String) 
 
 # convert to matrix of single chars
 report = let A = report_raw
@@ -14,9 +15,10 @@ end
 
 # --- pt 1 ---
 
-modes = mode.(eachcol(report))
-anti_modes = [Dict('0' => '1', '1' => '0')[key] for key in modes]
-modes_as_string = String.([modes, anti_modes])
+modes = map(mode, eachcol(report))
+anti_modes = map(bitchar -> Dict('0' => '1', '1' => '0')[bitchar], 
+                 modes)
+modes_as_string = map(String, [modes, anti_modes])
 # bits to ints (Julia greek compatibility, baybee)
 γ, ε = parse.(Int, modes_as_string, base=2)
 
@@ -24,3 +26,25 @@ answer_1 = prod([γ, ε])
 
 
 # --- pt 2 ---
+
+function compound_rating(readout, compound)
+    subreadout = readout
+    ncols = size(subreadout)[2]
+    jj = 1
+    while size(subreadout)[1] > 1
+        repcol = subreadout[:, jj]
+        if compound == "oxygen" 
+            keeps = repcol .== tiebreak_mode(repcol)
+        elseif compound == "co2"
+            keeps = repcol .!= tiebreak_mode(repcol)
+        end
+        subreadout = subreadout[keeps, :]
+        # resolve indexing
+        jj +=1 
+        jj = jj <= ncols ? jj : 1
+    end
+    return parse(Int, subreadout |> vec |> String, base=2)
+end
+
+prod([compound_rating(report, "oxygen") compound_rating(report, "co2")])
+
